5a3fba74b3c26b38398caee252d27be4509a0b3d
Naoya Yamashita <conao3@gmail.com>
2024-01-07 05:03:59 +0900

    implement cedar binding

--- tests/test_maturin_cedar.py
+++ tests/test_maturin_cedar.py
@@ -8,3 +8,20 @@ def test_sum_as_string():
 def test_list_sum_as_string():
     assert test_maturin_cedar.lib.list_sum_as_string(1, 3, 2, 4) == ["4", "6"]
     assert test_maturin_cedar.lib.list_sum_as_string(1, 3, 2) == ["4", "2"]
+
+
+def test_cedar_simple():
+    request = (
+        'User::"alice"',
+        'Action::"update"',
+        'Photo::"VacationPhoto94.jpg"',
+    )
+    policy_set = """
+permit(
+  principal == User::"alice",
+  action    == Action::"update",
+  resource  == Photo::"VacationPhoto94.jpg"
+);
+"""
+    authorizer = test_maturin_cedar.Authorizer()
+    assert authorizer.is_authorized(request, policy_set) == True
